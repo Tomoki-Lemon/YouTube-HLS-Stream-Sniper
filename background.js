c9d57d64f1981d8f4377cda5123a4096c14fb77f
Avi Patil <58633270+proavipatil@users.noreply.github.com>
2020-10-03 16:35:55 +0530

    Update background.js

--- background.js
+++ background.js
@@ -21,10 +21,12 @@ const listenerFilter = {
 		"*://*/*.mp3?*",
 		"*://*/*.srt",
 		"*://*/*.srt?*",
-    "*://*/*.m4a",
+                "*://*/*.m4a",
 		"*://*/*.m4a?*",
 		"*://*/*.webm",
-		"*://*/*.webm?*"
+		"*://*/*.webm?*",
+		"*://*/*.avi",
+		"*://*/*.avi?*"
 	]
 };
 
@@ -44,6 +46,122 @@ function addURL(requestDetails) {
 		) {
 			browser.browserAction.setBadgeBackgroundColor({ color: "green" });
 			badgeText += 1;
+			browser.browserAction.setBadgeText({
+				text: badgeText.toString()
+			});
+
+			const url = new URL(requestDetails.url);
+
+			let filename;
+			if (url.href.indexOf(".ism") === -1) {
+				filename = url.href.slice(
+					url.href.lastIndexOf("/") + 1,
+					url.href.lastIndexOf(".") || url.href.lastIndexOf("?")
+				);
+				filename = filename.slice(
+					0,
+					filename.indexOf("?") === -1 ? filename.length : filename.indexOf("?")
+				); // why does slice have to be the way it is
+			} else {
+				filename = url.href.slice(
+					url.href.lastIndexOf("/", url.href.lastIndexOf(".ism")) + 1,
+					url.href.lastIndexOf(".")
+				);
+			}
+
+			let ext = url.href.slice(url.href.lastIndexOf(".") + 1, url.href.length);
+			ext = ext.slice(
+				0,
+				ext.lastIndexOf("/") === -1 ? ext.length : ext.lastIndexOf("/")
+			);
+			ext = ext.slice(
+				0,
+				ext.lastIndexOf("?") === -1 ? ext.length : ext.lastIndexOf("?")
+			);
+
+			const { hostname } = url;
+
+			const timestamp = Date.now();
+
+			const newRequestDetails = {
+				...requestDetails,
+				filename,
+				ext,
+				hostname,
+				timestamp
+			};
+
+			urlStorage.push(newRequestDetails); // the following promise is too slow - workaround instead of doing it properly
+			if (
+				options.urlStorage.length !== urlStorage.length &&
+				options.badgeText !== badgeText
+			)
+				browser.storage.local.set({ urlStorage, badgeText }).then(() => {
+					browser.runtime.sendMessage({ urlStorage: true }); // update popup if opened
+				});
+
+			if (options.notifPref !== true) {
+				browser.notifications.create("add", {
+					// id = only one notification of this type appears at a time
+					type: "basic",
+					iconUrl: "img/icon-dark-96.png",
+					title: _("notifTitle"),
+					message: `${_("notifText") + filename}.${ext}`
+				});
+			}
+		}
+	});
+}
+
+function deleteURL(message) {
+	// url deletion
+	urlStorage = urlStorage.filter(
+		url => !message.map(msgUrl => msgUrl.requestId).includes(url.requestId)
+	);
+	badgeText = urlStorage.filter(url => !url.restore).length;
+
+	browser.storage.local.set({ urlStorage, badgeText }).then(() => {
+		browser.runtime.sendMessage({ urlStorage: true });
+		browser.browserAction.setBadgeText({
+			text: badgeText === 0 ? "" : badgeText.toString() // only display at 1+
+		});
+	});
+}
+
+function setup() {
+	// clear everything and/or set up
+	browser.browserAction.setBadgeText({ text: "" });
+
+	browser.storage.local.get().then(options => {
+		browser.storage.local
+			.set({
+				disablePref: !options.disablePref ? false : options.disablePref,
+				urlStorage: !options.urlStorage ? [] : options.urlStorage
+			})
+			.then(() => {
+				browser.webRequest.onBeforeSendHeaders.addListener(
+					addURL,
+					listenerFilter,
+					["requestHeaders"]
+				);
+				if (options.urlStorage && options.urlStorage.length > 0) {
+					// restore urls on startup
+					for (const url of options.urlStorage) {
+						url.restore = true;
+						urlStorage.push(url);
+					}
+					browser.storage.local.set({ urlStorage });
+				}
+			});
+	});
+
+	browser.runtime.onMessage.addListener(message => {
+		if (message.delete) deleteURL(message.delete);
+	});
+}
+
+setup();
+
 			browser.browserAction.setBadgeText({
 				text: badgeText.toString()
 			});
